76893909bea410692d178430745090b65f80e850
Ramesh Gurung <47062509+gurungr2@users.noreply.github.com>
2019-10-16 13:41:53 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Loader from './components/shared-components/Loader';
-
 ReactDOM.render(<Loader />, document.getElementById('root'));
-setTimeout(()=>{ReactDOM.render(<App />, document.getElementById('root'))}, 1000);
+setTimeout(()=>{ReactDOM.render(<App />, document.getElementById('root'))}, 10);
 serviceWorker.unregister();
